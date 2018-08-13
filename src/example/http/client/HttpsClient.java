93560c549cad4d213c547cbdfccc0043bdd6d475
jeff <sara.ngo.nga@clearpathdevelopment.com>
2018-08-13 11:24:34 +0700

    enhance httpsclient file

--- src/example/http/client/HttpsClient.java
+++ src/example/http/client/HttpsClient.java
@@ -6,7 +6,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,52 +19,55 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 public class HttpsClient {
 
+	String storePath = "G:\\SSD-CP\\D-hard-disk\\servers\\httpd-2.4.33-o102o-x86-vc14-r2\\Apache24\\htdocs\\ts-video-demo\\";
 	public static void main(String[] args) {
 		new HttpsClient().doTask();
 	}
 
 	private void doTask() {
-		//joinPartToMp4("temp");
+		// joinPartToMp4("temp");
 		// first: get m3u8 file
-		String m3u8Url = "https://storage.googleapis.com/hachium/streaming/6658b9fc85f4057438ff6ace48582306/1523430395552/360.m3u8";
-		String m3u8Content = removeEXTXKEY(getContentFile(m3u8Url));
-		writeStringToFile(m3u8Content, "temp/playlist.m3u8");
+		String m3u8Url = "https://storage.googleapis.com/hachium/streaming/6658b9fc85f4057438ff6ace48582306/1523430773559/360.m3u8";
+		String m3u8Content = getContentFile(m3u8Url);
+		String key = getContentFile("http://khoahoc.fedu.vn/lecture/get_streaming_key");
+		//m3u8Content = removeEXTXKEY(m3u8Content);
+		writeStringToFile(m3u8Content, storePath + "seglist_720p.m3u8");
 		List<String> tsList = collectTsFiles(m3u8Content);
 		if (tsList.size() > 0) {
 			String serverPath = m3u8Url.substring(0, m3u8Url.lastIndexOf('/'));
 			boolean isSuccess = downloadTsFiles(tsList, serverPath);
-			if(isSuccess) {
+			if (isSuccess) {
 				joinPartToMp4("temp");
 			}
 		}
 	}
-	
-	private boolean downloadTsFiles(List<String>tsList, String serverPath) {
+
+	private boolean downloadTsFiles(List<String> tsList, String serverPath) {
 		try {
 			for (int i = 0; i < tsList.size(); i++) {
 				String tsName = tsList.get(i);
-				HttpsURLConnection con = openConn(serverPath + "/" + tsName);
-				if (con != null) { 
-					writeStreamToFile(con, "temp/" + tsName);
+				Object con = httpConnFactory.getConn(serverPath + "/" + tsName);
+				if (con != null) {
+					writeStreamToFile(con, storePath + tsName);
 				}
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-	
+
 	private boolean joinPartToMp4(String tempPath) {
-		try{
-        	executeCommand("ffmpeg -i temp/playlist.m3u8 -c copy -bsf:a aac_adtstoasc temp/output.mp4");
-    	}catch(Exception e){
-    		return false;
-    	}
+		try {
+			executeCommand("ffmpeg -i temp/playlist.m3u8 -c copy -bsf:a aac_adtstoasc temp/output.mp4");
+		} catch (Exception e) {
+			return false;
+		}
 		return true;
 	}
-	
-	private String executeCommand(String command) throws Exception{
+
+	private String executeCommand(String command) throws Exception {
 
 		StringBuffer output = new StringBuffer();
 
@@ -70,11 +75,10 @@ public class HttpsClient {
 		try {
 			p = Runtime.getRuntime().exec(command);
 			p.waitFor();
-			BufferedReader reader = 
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-                        String line = "";			
-			while ((line = reader.readLine())!= null) {
+			String line = "";
+			while ((line = reader.readLine()) != null) {
 				output.append(line + "\n");
 				System.out.println(line + "\n");
 			}
@@ -86,7 +90,7 @@ public class HttpsClient {
 		return output.toString();
 
 	}
-	
+
 	private String removeEXTXKEY(String m3u8Content) {
 		// first: get m3u8 file
 		String[] parts = m3u8Content.split("\n");
@@ -100,6 +104,18 @@ public class HttpsClient {
 		return String.join("\n", tsList);
 	}
 	
+	private String getEXTXKEY(String m3u8Content) {
+		// first: get m3u8 file
+		String[] parts = m3u8Content.split("\n");
+		for (int i = 0; i < parts.length; i++) {
+			String part = parts[i];
+			if (!part.contains("EXT-X-KEY")) {
+				return part;
+			}
+		}
+		return "";
+	}
+
 	private List<String> collectTsFiles(String m3u8Content) {
 		// first: get m3u8 file
 		String[] parts = m3u8Content.split("\n");
@@ -113,27 +129,15 @@ public class HttpsClient {
 		return tsList;
 	}
 
-	private HttpsURLConnection openConn(String path) {
-		try {
-			URL url = new URL(path);
-			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-			return con;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
 	private String getContentFile(String path) {
 
 		String content = "";
-		HttpsURLConnection con = openConn(path);
+		Object con = httpConnFactory.getConn(path);
 		if (con != null) {
 			System.out.println("****** Content of the URL ********");
 			BufferedReader bufferedReader = null;
 			try {
-				bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				bufferedReader = new BufferedReader(new InputStreamReader(httpConnFactory.getInputStream(con)));
 				String s = "";
 				while ((s = bufferedReader.readLine()) != null) {
 					content += s + "\n";
@@ -149,7 +153,7 @@ public class HttpsClient {
 		}
 		return content;
 	}
-	
+
 	private boolean writeStringToFile(String content, String path) {
 		try {
 			File file = new File(path);
@@ -164,11 +168,11 @@ public class HttpsClient {
 		return true;
 	}
 
-	private void writeStreamToFile(HttpsURLConnection con, String path) {
+	private void writeStreamToFile(Object con, String path) {
 		BufferedReader bufferedReader = null;
 		BufferedWriter bufferedWriter = null;
 		try {
-			bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			bufferedReader = new BufferedReader(new InputStreamReader(httpConnFactory.getInputStream(con)));
 			bufferedWriter = new BufferedWriter(new FileWriter(path));
 			String s = "";
 			while ((s = bufferedReader.readLine()) != null) {
@@ -186,6 +190,43 @@ public class HttpsClient {
 			// close without throwing exception
 			IOUtils.closeQuietly(bufferedReader);
 			IOUtils.closeQuietly(bufferedWriter);
+		}
+
+	}
+
+	static class httpConnFactory {
+
+		public static Object getConn(String path) {
+
+			try {
+				URL url = new URL(path);
+				if (path.indexOf("https") != -1) {
+					return (HttpsURLConnection) url.openConnection();
+				} else {
+					return (HttpURLConnection) url.openConnection();
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+
+		}
+		
+		public static InputStream getInputStream(Object conn) {
+
+			try {
+				if (conn instanceof HttpsURLConnection) {
+					return ((HttpsURLConnection) conn).getInputStream();
+				} else {
+					return ((HttpURLConnection) conn).getInputStream();
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+
 		}
 
 	}
